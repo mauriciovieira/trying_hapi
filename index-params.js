c3e76fdc220f42633c82005aa97b8288f4b5ccf3
Mauricio Vieira <mauricio@mauriciovieira.net>
2016-02-28 18:22:51 -0300

    Params behaviors

--- index-params.js
+++ index-params.js
@@ -8,11 +8,46 @@ function handler(request, reply) {
   reply(request.params)
 }
 
+/* Less specific */
+server.route({
+  method: 'GET',
+  path: '/{stuff*}',
+  handler: handler
+})
+
+/* Most specific */
+server.route({
+    method: 'get',
+    path: '/users/{userid?}',
+    handler: handler
+})
+
+server.route({
+    method: 'get',
+    path: '/users/{userid}/files',
+    handler: handler
+})
+
+/* Match */
 server.route({
     method: 'GET',
-    path: '/users/{userId?}',
+    path: '/files/{files}.jpg',
     handler: handler
-  })
+})
+
+/* Wildcard */
+server.route({
+    method: 'GET',
+    path: '/files/{files*}',
+    handler: handler
+})
+
+/* Wildcard fixed */
+server.route({
+    method: 'GET',
+    path: '/folders/{folders*2}',
+    handler: handler
+})
 
 server.start(() => console.log(`Started at: ${server.info.uri}`))
 
